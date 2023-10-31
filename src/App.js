88892e8436b200df778970845e060be80c54e5c8
JesusRestrepo <jesusrestrepo2003@gmail.com>
2023-10-31 06:13:18 -0500

    finalizando proyecto

--- src/App.js
+++ src/App.js
@@ -1,41 +1,96 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Bars from "./components/BarsChart";
-import jsonData from './data.json';
+import jsonData from "./data.json";
 
 import "./App.css";
-import { obtenerClasificacion, obtenerPrueba, obtenerScraping } from "./utils/api";
+import {
+  obtenerClasificacion,
+  obtenerPrueba,
+  obtenerScraping,
+} from "./utils/api";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+var misoptions = {
+  responsive: true,
+  animation: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+  scales: {
+    y: {
+      min: 0,
+      max: 15,
+    },
+    x: {
+      ticks: { color: "black" },
+    },
+  },
+};
 
 function App() {
-  const [prueba, setPrueba] = useState([]);
+  const [prueba, setPrueba] = useState(false);
   const [mostrarClasificacion, setMostrarClasificacion] = useState(false);
-  const [titulo, setTitulo] = useState('');
-  const [categoria, setCategoria] = useState('');
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
   const [charData, setChartData] = useState({});
-
-  const [currentInput, setCurrentInput] = useState('');
+  const [inputJson, setInputJson] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setChartData(jsonData);
   }, []);
 
   const scraping = async () => {
+    setLoading(true);
     try {
       const response = await obtenerScraping();
       console.log(response);
-      setPrueba([1]);
+      setInputJson(response || {}); // Manejar el caso en que response sea undefined o null
+      console.log(inputJson);
+      setPrueba(true);
+      setLoading(false);
     } catch (err) {
       console.error(err);
+      setPrueba(false);
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    console.log(inputJson);
+  }, [inputJson]);
 
   const handleSearchClick = () => {
     searchCategoria(titulo);
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       searchCategoria(titulo);
     }
   };
@@ -50,20 +105,22 @@ function App() {
       const response = await obtenerClasificacion(titulo);
       const resultados = response.resultados;
       console.log(resultados);
-      if (resultados == 1){
-        setCategoria("Desarrollo y calidad del software")
-        setMostrarClasificacion(true)
-      } else if (resultados == 2){
-        setCategoria("Infraestructura y Operaciones")
-        setMostrarClasificacion(true)
-      } else if(resultados == 3){
-        setCategoria("Especialistas en ciberseguridad")
-        setMostrarClasificacion(true)
-      } else if (resultados == 4){
-        setCategoria("Gestión de datos y bases de datos")
-        setMostrarClasificacion(true) 
-      } else{
-        setCategoria("No se encontró una coincidencia con el titulo ingresado.")
+      if (resultados === 1) {
+        setCategoria("Desarrollo y calidad del software");
+        setMostrarClasificacion(true);
+      } else if (resultados === 2) {
+        setCategoria("Infraestructura y Operaciones");
+        setMostrarClasificacion(true);
+      } else if (resultados === 3) {
+        setCategoria("Gestión de datos y bases de datos");
+        setMostrarClasificacion(true);
+      } else if (resultados === 4) {
+        setCategoria("Especialistas en ciberseguridad");
+        setMostrarClasificacion(true);
+      } else {
+        setCategoria(
+          "No se encontró una coincidencia con el título ingresado."
+        );
       }
     } catch (error) {
       console.error(error);
@@ -75,22 +132,39 @@ function App() {
     try {
       // Realiza la solicitud a la API con el título
       const response = await obtenerPrueba(titulo);
-  
+
       // Genera el enlace de descarga si la solicitud tiene éxito
-      const pdfBlob = new Blob([response], { type: 'application/pdf' });
+      const pdfBlob = new Blob([response], { type: "application/pdf" });
       const url = window.URL.createObjectURL(pdfBlob);
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', `${titulo}.pdf`);
+      link.setAttribute("download", `${titulo}.pdf`);
       document.body.appendChild(link);
       link.click();
-  
+
       return response; // Puedes devolver los datos adicionales de la API si es necesario
     } catch (error) {
       console.error(error);
       return Promise.reject(error);
     }
-  }
+  };
+
+  const labels = Object.values(inputJson.titulo || {}); // Manejar el caso en que inputJson.titulo sea undefined o null
+  console.log("labels", labels);
+
+  const data = Object.values(inputJson.cantidad || {}); // Manejar el caso en que inputJson.cantidad sea undefined o null
+  console.log("dataa", data);
+
+  const transformedJson = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Solicitudes",
+        data: data,
+        backgroundColor: "red",
+      },
+    ],
+  };
 
   return (
     <>
@@ -107,49 +181,66 @@ function App() {
             <button id="btn-scraping" onClick={scraping}>
               Comenzar web scraping
             </button>
-          </div><br></br>
-          {prueba.length > 0 ? (
+          </div>
+          <br></br>
+          {loading ? (
+            <p>Cargando</p>
+          ) : (
             <>
-              <div className="content-info">
-                <Bars/>
-                <p>A continuación, en el recuadro de abajo digita el título que deseas</p><br></br>
-              </div>
-              {mostrarClasificacion === true ? (
-                  <>
-                    <div className="content-info">
-                      <p>El titulo {titulo} lo clasificamos como: {categoria}</p>
-                      <button onClick={handleGenerarPruebas} id="btn-scraping">Generar prueba</button>
-                    </div><br></br>
-                    <div className="content-info">
-                      {/* <PDFViewer/> */}
+              {prueba ? 
+              (
+                <>
+                  <div className="content-info">
+                    <Bar data={transformedJson} options={misoptions} />
+                    <p>
+                      A continuación, en el recuadro de abajo digita el título
+                      que deseas
+                    </p>
+                    <br></br>
+                  </div>
+                  {mostrarClasificacion === true ? (
+                    <>
+                      <div className="content-info">
+                        <p>
+                          El titulo {titulo} lo clasificamos como: {categoria}
+                        </p>
+                        <button
+                          onClick={handleGenerarPruebas}
+                          id="btn-scraping"
+                        >
+                          Generar prueba
+                        </button>
+                      </div>
+                      <br></br>
+                      <div className="content-info">{/* <PDFViewer/> */}</div>
+                    </>
+                  ) : null}
+                  <div className="content-input">
+                    <div className="text-input">
+                      <Box
+                        component="form"
+                        sx={{
+                          "& .MuiTextField-root": { m: 1, width: "100%" },
+                        }}
+                      >
+                        <TextField
+                          id="standard-basic"
+                          type="text"
+                          onKeyPress={handleKeyPress}
+                          onChange={handleInputChange}
+                          placeholder="Escribe el título"
+                          variant="filled"
+                        />
+                      </Box>
                     </div>
-                  </>
-                ):null}
-              <div className="content-input">
-                <div className="text-input">
-                  <Box
-                    component="form"
-                    sx={{
-                      "& .MuiTextField-root": { m: 1, width: "100%" },
-                    }}
-                  >
-                    <TextField
-                      id="standard-basic"
-                      type="text"
-                      value={titulo}
-                      onKeyPress={handleKeyPress}
-                      onChange={handleInputChange}
-                      placeholder="Escribe el título"
-                      variant="filled"
-                    />
-                  </Box>
-                </div>
-                <div className="boton-text-input">
-                  <button onClick={handleSearchClick}>></button>
-                </div>
-              </div>
+                    <div className="boton-text-input">
+                      <button onClick={handleSearchClick}>></button>
+                    </div>
+                  </div>
+                </>
+              ) : null}
             </>
-          ) : null}
+          )}
         </div>
       </section>
     </>
